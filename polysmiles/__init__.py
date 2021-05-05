06aeafdadaecd448907d3b45baefec40ae3ab51d
KanHatakeyama <satokan@toki.waseda.jp>
2021-05-05 14:25:13 +0900

    Update __init__.py

--- polysmiles/__init__.py
+++ polysmiles/__init__.py
@@ -1,1 +1,7 @@
-import polysmiles.read_smiles
+from .JRWrapper import *
+from .polymer_graph_helper import *
+from .PolySMILES import *
+from .read_smiles import *
+from .smiles_helper import *
+from .write_smiles import *
+from .AutoDescriptor import *
